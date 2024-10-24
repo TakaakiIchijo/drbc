9ceaec8c5edbe06b510111b85e92d81f293f0c6d
ichijo <ichijo@head-high.com>
2024-10-24 23:04:14 +0900

    removeunusedassets

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "com.Head-High-DRBC-0.1.0_2024-1024-2243";
+const cacheName = "com.Head-High-DRBC-0.1.0_2024-1024-2303";
 const contentToCache = [
     "Build/drbc.loader.js",
     "Build/drbc.framework.js.unityweb",
