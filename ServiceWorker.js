d2e790dbd8d7ab93aebb21487fc0cc35cefe1268
ichijo <ichijo@head-high.com>
2024-10-29 11:14:38 +0900

    addbadwordfilter

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "com.Head-High-Companion DRB-0.1.0_2024-1024-2352";
+const cacheName = "com.Head-High-Companion DRB-0.1.0_2024-1029-1107";
 const contentToCache = [
     "Build/drbc.loader.js",
     "Build/drbc.framework.js.unityweb",
